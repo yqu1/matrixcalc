a75970d74efdc5809e6b72141f088501204a6131
yqu1 <quyaoxian@gmail.com>
2015-11-20 11:05:13 -0500

    second commit

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+void QRdecomp();
+void LUdecomp();
+void determinant();
+
 int main()
 {
 	char option;
@@ -19,11 +23,11 @@ int main()
 	while(option!='d')
 	{
 
-		if(option=='a') { LUdecomp(); }
+		if(option=='a')  LUdecomp(); 
 
-		else if(option=='b') { QRdecomp(); }
+		else if(option=='b')  QRdecomp(); 
 
-		else if(option=='c') { determinant(); }
+		else if(option=='c')  determinant(); 
 
 		else { cout<<"invalid input"; }
 
@@ -41,5 +45,97 @@ int main()
 	cout<<"thank you for using matrix calculator!"<<endl;
 
 	return 0;
+
+}
+
+void QRdecomp()
+{
+    int m,n;
+
+    cout<<"enter matrix rows: ";
+    cin>>m;
+    cout<<"enter matrix columns: ";
+    cin>>n;
+
+    double temp[m*n];
+
+    for (int i=0; i<(m*n); i++)
+    {
+        cout<<"enter element: "<<endl;
+        cin>>temp[i];
+    }
+
+
+    Matrix<double> A__(m,n,temp);
+    Matrix<double> Q__(1,1), R__(1,1);
+    A__.householderDecomposition(Q__, R__);
+
+    cout<<"matrix you entered is: "<<endl;
+    A__.output();
+    cout<<"Q matrix: "<<endl;
+    Q__.output();
+    cout<<"R matrix: "<<endl;
+    R__.output();
+    cout<<"Q*R: "<<endl;
+    (Q__*R__).output();
+
+
+}
+
+void LUdecomp()
+{
+
+    int n = 0, i = 0, j = 0;
+    cout << "Enter size of Square matrix : ";
+    cin >> n;
+    double temp[n*n], l[n*n], u[n*n];
+
+    for (i = 0; i < n*n; i++)
+    {
+        cout << "Enter element: "<<endl;
+        cin >> temp[i];
+    }
+
+    Matrix<double> A__(n,n,temp);
+    Matrix<double> L__(n,n), U__(n,n);
+    L__.zero(); U__.zero();
+
+    cout<<"matrix you entered is: "<<endl;
+    A__.output();
+
+    if (A__.determinant()!=0)
+    {
+        cout<<"determinant is: "<<A__.determinant()<<endl;
+        A__.lu(L__, U__);
+        cout << "\nL Decomposition\n\n";
+        L__.output();
+        cout << "\nU Decomposition\n\n";
+        U__.output();
+        cout<<"L*U: "<<endl;
+        (L__*U__).output();
+    }
+
+    else {cout<<"matrix is singular"<<endl;}
+}
+
+void determinant()
+{
+    int n = 0, i = 0, j = 0;
+    cout << "Enter size of Square matrix : ";
+    cin >> n;
+    double temp[n*n], l[n*n], u[n*n];
+
+    for (i = 0; i < n*n; i++)
+    {
+        cout << "Enter element: "<<endl;
+        cin >> temp[i];
+    }
+
+    Matrix<double> A__(n,n,temp);
+
+    cout<<"matrix you entered is: "<<endl;
+    A__.output();
+
+    cout<<"determinant is: "<<A__.determinant()<<endl;
 
 }
